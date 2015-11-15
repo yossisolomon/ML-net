5827d7ba04617222f88f774c3c944c01bc51d9ef
Yossi Solomon <yosisolomon@gmail.com>
2015-11-15 02:55:12 +0200

    New decode to create 1 CSV per experiment

--- decode.py
+++ decode.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 
 import os
+from datetime import datetime
+import logging
 
 startDatagram ='startDatagram =================================\n'
 endDatagram ='endDatagram   =================================\n'
 startSample = 'startSample ----------------------\n'
 endSample = 'endSample   ----------------------\n'
 disregardCounter = ['sampleType_tag','sourceId', 'counterBlock_tag', 'networkType', 'ifSpeed', 'ifDirection', 'ifStatus', 'ifInBroadcastPkts','ifInUnknownProtos','ifOutMulticastPkts','ifOutBroadcastPkts','ifPromiscuousMode']
-overloadByteRate = 1100000
+relevantKeys = ['ifInOctets','ifInUcastPkts','ifOutOctets','ifOutUcastPkts']
+loadersIfNames = ['s0-eth1','s1-eth1','s2-eth1']
+destIfName = 's3-eth1'
+deltasErrorMargin = 0.05
+mega = pow(2,20)
+megaByte = mega/8
+overloadByteRate = 10*megaByte*0.7
 
 
 def get_interfaces_to_names_map():
@@ -62,44 +70,77 @@ def get_key_value_from_line(line, delim=' '):
     return split[0] , split[1]
 
 
-def create_interface_csv_files(interfaces_to_names, datagrams):
-    names_to_samples = {}
+def get_relevant_sampling(sample):
+    new_sample = {}
+    for k in relevantKeys:
+        new_sample[k] = sample[k]
+    return new_sample
+
+
+def is_sampling_size_ok(index_to_samples):
+    sampling_counters = [len(s) for s in index_to_samples.values()]
+    if len(set(sampling_counters)) > 1:
+        return False
+    return True
+
+
+def is_deltas_sampling_ok(loaders_tot_delta,dest_delta):
+    if abs(loaders_tot_delta-dest_delta) > deltasErrorMargin*dest_delta:
+        return False
+    return True
+
+
+def create_sampling_csv_file(index_to_samples,sorted_if_names):
+    with open('/tmp/sflowCSV-%s'%datetime.now().isoformat(),'w') as f:
+        for i in index_to_samples.keys():
+            values = []
+            loaders_tot_delta = 0
+            dest_delta = 0
+            for eth in sorted_if_names:
+                for k in relevantKeys:
+                    values.append(index_to_samples[i][eth][k])
+                    curr = index_to_samples[i][eth][k]
+                    # insert delta for counter
+                    # (take delta from 0 if this is the first sample)
+                    delta = curr if i=='1' else str(int(curr)-int(index_to_samples[str(int(i)-1)][eth][k]))
+                    if eth in loadersIfNames and k == 'ifInOctets':
+                        logging.info("delta for " + eth + " :" + delta)
+                        loaders_tot_delta += int(delta)
+                    elif eth == destIfName and k == 'ifInOctets':
+                        dest_delta = int(delta)
+                    values.append(delta)
+            tag = '1' if loaders_tot_delta >= overloadByteRate else '0'
+            if not is_deltas_sampling_ok(loaders_tot_delta,dest_delta):
+                logging.warn('Deltas margin of error (%s) passed for sample#%s: loadTot=%s   dest=%s'%(deltasErrorMargin,i,loaders_tot_delta,dest_delta))
+            logging.info('tag='+tag)
+            f.write(', '.join([tag] + values) + os.linesep)
+
+
+def get_index_to_samples_map(datagrams, interfaces_to_names):
+    index_to_samples = {}
     for d in datagrams:
         for s in d['samples']:
-            # only accept counter samples
-            if s['sampleType'] == 'COUNTERSSAMPLE':
+            # only accept counter samples which exist in the name mapping
+            if s['sampleType'] == 'COUNTERSSAMPLE' and interfaces_to_names.has_key(s['ifIndex']):
                 name = interfaces_to_names[s['ifIndex']]
-                if not names_to_samples.has_key(name):
-                    names_to_samples[name] = [s]
+                index = s['sampleSequenceNo']
+                if not index_to_samples.has_key(index):
+                    index_to_samples[index] = {name:get_relevant_sampling(s)}
                 else:
-                    names_to_samples[name].append(s)
-    for name, samples in names_to_samples.iteritems():
-        with open('/tmp/sflowCSV-'+name,'w') as f:
-            prevInOctets = 0
-            prevOutOctets = 0
-            for s in samples:
-                deltaInOctets = int(s['ifInOctets']) - prevInOctets
-                deltaOutOctets = int(s['ifOutOctets']) - prevOutOctets
-                values = [s['sampleSequenceNo'],
-                          s['ifInOctets'],
-                          s['ifInUcastPkts'],
-                          str(deltaInOctets),
-                          "overloadIn" if deltaInOctets > overloadByteRate else "normalIn",
-                          s['ifOutOctets'],
-                          s['ifOutUcastPkts'],
-                          str(deltaOutOctets),
-                          "overloadOut" if deltaOutOctets > overloadByteRate else "normalOut"]
-                f.write(', '.join(values) + os.linesep)
-                prevInOctets = int(s['ifInOctets'])
-                prevOutOctets = int(s['ifOutOctets'])
-
-
+                    index_to_samples[index][name] = get_relevant_sampling(s)
+    return index_to_samples
 
 
 def main():
+    logging.info("overload th: " + str(overloadByteRate))
     interfaces_to_names = get_interfaces_to_names_map()
+    relevant_interfaces = dict(filter(lambda (i,n): "-eth" in n and not "root" in n,interfaces_to_names.items()))
     datagrams = get_datagrams()
-    create_interface_csv_files(interfaces_to_names, datagrams)
+    index_to_samples = get_index_to_samples_map(datagrams, relevant_interfaces)
+    if not is_sampling_size_ok(index_to_samples):
+        raise Exception("bad sampling")
+    sorted_if_names = sorted(relevant_interfaces.values())
+    create_sampling_csv_file(index_to_samples,sorted_if_names)
 
 
 if __name__ == '__main__':
