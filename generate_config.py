0ec1cbcb09d238601a528b692fa8e7522edefdf1
Yossi Solomon <yosisolomon@gmail.com>
2015-11-16 02:20:02 +0200

    fix config generation bug (bytes for bits)

--- generate_config.py
+++ generate_config.py
@@ -10,6 +10,7 @@ expDurationMilliSeconds = 1000*expDurationSeconds
 periodLengthMilliSeconds = 30*1000
 periodSendLength = 10 * 1000
 
+#Not designed for more than two yet!
 loaderHosts = 2
 
 defaultPacketSize = 1024
@@ -29,13 +30,13 @@ def create_loader_hosts_commands(numHosts, periodDuration, totalDuration):
     for i in xrange(periods):
         for h in xrange(numHosts):
             delay = i * periodLengthMilliSeconds + random.randrange(1000,10000)
-            pps = random.randrange(1024,4608)
+            pps = random.randrange(128,576)
             commands[h].append(create_command(baseCommand,periodSendLength,delay,pps,defaultPacketSize))
     return commands
 
 
 
-steadyStateHostCommand = create_command(baseCommand, expDurationMilliSeconds, 0, 1024, defaultPacketSize)
+steadyStateHostCommand = create_command(baseCommand, expDurationMilliSeconds, 0, 128, defaultPacketSize)
 loaderHostsCommands = create_loader_hosts_commands(loaderHosts,periodLengthMilliSeconds,expDurationMilliSeconds)
 print "steady:"
 print steadyStateHostCommand
