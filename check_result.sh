2baa2574420ec399ae2421e468ddf8b7a606be49
Yossi Solomon <yosisolomon@gmail.com>
2015-11-17 14:19:30 -0800

    Add CSV result checker script

---
+++ check_result.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+overload=$(cat $1 | cut -d, -f1 | paste -sd+ | bc)
+total=$(cat $1 | cut -d, -f1 | wc -l)
+echo Total=$total Overload=$overload %=$(python -c "print $overload*1.0/$total")
+
