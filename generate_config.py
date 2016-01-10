f3887d2fe6545d9bcdfd8ae30d52fe82670ecfa0
Yossi Solomon <yosisolomon@gmail.com>
2016-01-10 02:13:08 +0200

    New configuration - steady loaders for rest of network

--- generate_config.py
+++ generate_config.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 import random
 import os
-baseCommand = "-m rttm -a 10.0.0.4"
-
+from netaddr import iter_iprange
+baseCommand = "-m rttm -a"
+loadedHost = "10.0.0.4"
+firstHost = "10.0.0.1"
+lastHost = '10.0.0.9'
 expDurationMinutes = 200
 expDurationSeconds = expDurationMinutes*60
 expDurationMilliSeconds = 1000*expDurationSeconds
@@ -15,8 +18,10 @@ loaderHosts = 2
 
 defaultPacketSize = 1024
 
-def create_command(base, duration, delay, pps, size):
+
+def create_command(base, dest, duration, delay, pps, size):
     cmd = base
+    cmd += " %s"%dest
     cmd += " -t %s"%duration
     cmd += " -d %s"%delay
     cmd += " -C %s"%pps
@@ -31,19 +36,30 @@ def create_loader_hosts_commands(numHosts, periodDuration, totalDuration):
         for h in xrange(numHosts):
             delay = i * periodLengthMilliSeconds + random.randrange(0,20000)
             pps = random.randrange(384,512)
-            commands[h].append(create_command(baseCommand,periodSendLength,delay,pps,defaultPacketSize))
+            commands[h].append(create_command(baseCommand,loadedHost,periodSendLength,delay,pps,defaultPacketSize))
     return commands
 
 
 
-steadyStateHostCommand = create_command(baseCommand, expDurationMilliSeconds, 0, 128, defaultPacketSize)
+steadyStateHostCommand = create_command(baseCommand,loadedHost, expDurationMilliSeconds, 0, 128, defaultPacketSize)
 loaderHostsCommands = create_loader_hosts_commands(loaderHosts,periodLengthMilliSeconds,expDurationMilliSeconds)
-print "steady:"
+print "steady loader:"
 print steadyStateHostCommand
 with open("./config-10.0.0.3","w") as f:
     f.write(steadyStateHostCommand+os.linesep)
-print "loaders:"
+print "dynamic loaders:"
 print loaderHostsCommands
 for h in xrange(loaderHosts):
     with open("./config-10.0.0.%s"%(h+1),"w") as f:
         map(lambda c: f.write(c+os.linesep),loaderHostsCommands[h])
+
+print "background loaders:"
+backgroundLoaders = iter_iprange(loadedHost, lastHost, step=1)
+for l in backgroundLoaders:
+    with open("./config-%s"%(l),"w") as f:
+        if str(l) == lastHost:
+            cmd = create_command(baseCommand, firstHost, expDurationMilliSeconds, 0, 128, defaultPacketSize)
+        else:
+            cmd = create_command(baseCommand,l+1, expDurationMilliSeconds, 0, 128, defaultPacketSize)
+        print cmd
+        f.write(cmd+os.linesep)
