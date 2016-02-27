80346827089fac6a1ec1ec08ab92e5ec4a645fd8
Yossi Solomon <yosisolomon@gmail.com>
2016-02-27 07:39:52 +0200

    add a watchdog script for ITGRecv Deals with failures during experiments

---
+++ ITGRecv.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Tiny script that re-lunches ITGRecv in case it dies...
+while [ 1 ]
+do
+	date=`date`
+
+	echo "-------------------------------"
+	echo "[$date] Starting ITGRecv"
+	ITGRecv -l /dev/null
+
+	echo "ITGRecv has crashed on: $date"
+
+	sleep 1
+done
