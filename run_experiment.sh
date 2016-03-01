f21e8ae5074a0ede707b87a9773e6e06aa9fd39d
Yossi Solomon <yosisolomon@gmail.com>
2016-03-01 23:14:37 +0200

    Run analysis as part of the experiment

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [[ -x $1 ]]
 then
     echo Running experiment with $1
@@ -18,3 +20,11 @@ sudo EXP_DIR=$EXP_DIR $1
 # cleanup mininet
 sudo mn -c
 
+ANALYSIS_DIR=$EXP_DIR/analysis
+mkdir -p $ANALYSIS_DIR
+~/ML-net/decode.py -i $EXP_DIR -o $ANALYSIS_DIR
+for csv in `ls $ANALYSIS_DIR/*.csv` ; do
+    echo Running SVM analysis for $csv ;
+    ~/ML-net/check_result.sh $csv ;
+    python ~/ML-net/switch_svm.py -i $csv --write-each-class-results &> $csv.result ;
+done
