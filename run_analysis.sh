291d6aced18e1a53df1cb6f3d4895b3fdd293cc5
Yossi Solomon <ysolomon@is-neptune-cri.cri.haifa.ac.il>
2016-03-12 03:58:16 +0200

    Ignoring ML analysis for unloaded ports

--- run_analysis.sh
+++ run_analysis.sh
@@ -14,9 +14,15 @@ ANALYSIS_DIR=$EXP_DIR/analysis
 mkdir -p $ANALYSIS_DIR
 ~/ML-net/decode.py -i $EXP_DIR -o $ANALYSIS_DIR &> $ANALYSIS_DIR/decode.log
 for csv in `ls $ANALYSIS_DIR/*.csv` ; do
-    echo Running SVM analysis for $csv ;
-    ~/ML-net/check_result.sh $csv ;
-    python ~/ML-net/switch_svm.py -i $csv --write-each-class-results &> $csv.result ;
+    echo Running SVM analysis for $csv 
+    CHECK=`~/ML-net/check_result.sh $csv`
+    echo $CHECK
+    if [ -z $(echo $CHECK | grep -q "Overload=0") ]
+    then
+      echo Skipping this file\'s ML analysis - no overloaded samples
+    else
+      python ~/ML-net/switch_svm.py -i $csv --write-each-class-results &> $csv.result 
+    fi
 done
 
 echo The analysis files can be found in $ANALYSIS_DIR
